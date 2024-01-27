bf56e1ff0ba829a45ebc48454f9656ba8baf4d1c
Ethan-Schledewitz-Edwards <144922288+Ethan-Schledewitz-Edwards@users.noreply.github.com>
2024-01-27 09:50:42 -0500

    Added some basic UI

--- Assets/Scripts/GameManagers/GameManager.cs
+++ Assets/Scripts/GameManagers/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,6 +20,16 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    [Header("System")]
+    private int currentLives;
+    private bool countingDown;
+    private float timeRemaining;
+
+    public event Action OnComplete;
+    public event Action OnFail;
+
+
+    #region Init Methods
     public static GameObject CreateSingletonManager()
     {
         var managerPrefab = Instantiate(Resources.Load<GameObject>("Managers/GameManager"));
@@ -29,5 +40,51 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
+
+        // Finish micro game subscribes to on OnComplete event
+        OnComplete += FinishMicroGame;
+
+        // Fail micro game subscribes to OnFail event
+        OnFail += FailMicroGame;
+    }
+    #endregion
+
+    private void Update()
+    {
+        if (countingDown)
+        {
+            timeRemaining -= Time.deltaTime;
+
+            if (timeRemaining <= 0)
+                FinishMicroGame();
+        }
+    }
+
+
+
+
+    /// <summary>
+    /// This method is used to start a micro game
+    /// </summary>
+    public void BeginMicroGame()
+    {
+        countingDown = true;
+    }
+
+
+    /// <summary>
+    /// This method should be called when a player completes a micro game.
+    /// </summary>
+    public void FinishMicroGame()
+    {
+        countingDown = false;
+    }
+
+    /// <summary>
+    /// This method should be called when a player fails a micro game.
+    /// </summary>
+    public void FailMicroGame()
+    {
+        countingDown = false;
     }
 }
